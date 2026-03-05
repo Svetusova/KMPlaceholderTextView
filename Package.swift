88818b220c45fd0d02d6c96a19f1a85f58e82bcb
Svet M <svetusova@gmail.com>
2026-03-05 21:15:44 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
-// swift-tools-version:5.3                                                                                                                          
-  import PackageDescription                                                                                                                           
+// swift-tools-version:5.3
+
+import PackageDescription                                                                                                                           
                                                                                                                                                       
-  let package = Package(                                                                                                                              
-      name: "KMPlaceholderTextView",                                                                                                                  
-      platforms: [                                                                                                                                    
-          .iOS(.v12)
-      ],
-      products: [
-          .library(
-              name: "KMPlaceholderTextView",
-              targets: ["KMPlaceholderTextView"]),
-      ],
-      targets: [
-          .target(
-              name: "KMPlaceholderTextView",
-              path: "Sources/KMPlaceholderTextView",
-              exclude: ["Info.plist"]),
-      ],
-      swiftLanguageVersions: [.v5]
-  )
+let package = Package(                                                                                                                              
+    name: "KMPlaceholderTextView",                                                                                                                  
+    platforms: [                                                                                                                                    
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "KMPlaceholderTextView",
+            targets: ["KMPlaceholderTextView"]),
+    ],
+    targets: [
+        .target(
+            name: "KMPlaceholderTextView",
+            path: "Sources/KMPlaceholderTextView",
+            exclude: ["Info.plist"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
